28ea48d13c2bc394e09adab1f74fc9d125b14694
Jeff Golden <gjgolden08@gmail.com>
2020-11-13 00:01:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(numbers)
   counter = 0 
   new_array = []
   while counter < numbers.length do 
-    numbers.index[counter]**numbers.index[counter]
+    new_array.push()
     counter += 1 
   end
 end
